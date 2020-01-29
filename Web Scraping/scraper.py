984043aad57a6fc50e723c33530614e7346803d3
Rho-Lall <rlall@dc2.propsol.org>
2020-01-28 17:21:14 -0700

    3.0 Update to Scraping Tool

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -4,33 +4,22 @@ import csv
 import re
 url = "https://www.census.gov/programs-surveys/popest.html"
 
-# send and catch the url request
 r = requests.get(url)
 
-# capture the html code
 html_code = r.text
 
 soup = BeautifulSoup(html_code, 'html.parser')
+
 links_set = set()
 
-for item in soup.find_all('a',href=re.compile(r'html')):  
+for item in soup.find_all('a', href=re.compile(r'^(http|/.)')):
     links_set.add(item.get('href'))
 
-URIs = [x for x in links_set if x[:4]=='http']
+clean_URIs = [x for x in links_set if x[:4]=='http']
 edited_URIs = ["https://www.census.gov" + x for x in links_set if x[:1]=='/']
-final_URIs = URIs + edited_URIs
+final_URIs = clean_URIs + edited_URIs
 
-if "https://www.census.gov/programs-surveys/popest.html" in final_URIs:
-    final_URIs = final_URIs.remove("https://www.census.gov/programs-surveys/popest.html")
-    
-if "http://www.census.gov/programs-surveys/popest.html" in final_URIs:
-    final_URIs = final_URIs.remove("https://www.census.gov/programs-surveys/popest.html")
-
-final_set = set()
-
-for item in final_URIs:  
-    final_set.add(item)
 
 with open("deliverable.csv", "w") as csv_file:
     writer = csv.writer(csv_file,delimiter="\n")
-    writer.writerow(final_set)   
+    writer.writerow(final_URIs)
